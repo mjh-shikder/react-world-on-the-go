4f19a1200c6e8339adf64cf54216f76b176f818e
mjh-shikder <mjhsikder@gmail.com>
2025-09-23 19:47:54 +0600

    after frist deploy

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -24,7 +24,7 @@ const Countries = ({ countriesPromise }) => {
 
   return (
     <div>
-          <h1>In the Countries: {countries.length} </h1>
+          <h1>Visited Countries: {countries.length} </h1>
           <h2>Total Country Visited: {vesitedCountryies.length} </h2>
           <h2>Total Flags Visited:{visitedFlags.length} </h2>
           <ol>
